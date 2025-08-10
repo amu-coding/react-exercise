7f66a0bb46776ff8c732a05136f31ca8c7bbe0e8
Abdulhay Muslim <amu-coding@users.noreply.github.com>
2025-08-10 17:02:19 +0430

    structure of an ID card created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <img src="../uploads/profilepic" alt="profile"></img>
+      <h1>Hi, I'm <span>Shiba</span></h1>
+      <p className="role">Front-End Web Developer - Afghanistan</p>
+      <p className="desc">Specializes in creating responsive, user-friendly interfaces using HTML, CSS, and JavaScript. Focused on delivering seamless user experiences through modern web technologies.</p>
+      <div className="social">
+        <div>
+          <img src="../uploads/github" alt="github icon"></img>
+        </div>
+        <div>
+          <img src="uploads/twitter" alt="twitter icon"></img>
+        </div>
+        <div>
+          <img src="uploads/tiktok" alt="tiktok icon"></img>
+        </div>
+        <div>
+          <img src="uploads/whatsapp" alt="whatsapp icon"></img>
+        </div>
+      </div>
     </div>
   );
 }
