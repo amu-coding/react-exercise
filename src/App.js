8554e9231eb372f4f72adb0568493b3e1ae56741
Abdulhay Muslim <amu-coding@users.noreply.github.com>
2025-08-10 17:43:01 +0430

    ID card design finalized in app.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import './App.css';
+import profilepic from './uploads/profilepic.jpg';
+import github from './uploads/github.png';
+import twitter from './uploads/twitter.png';
+import tiktok from './uploads/tiktok.png';
+import whatsapp from './uploads/whatsapp.png';
 
 function App() {
   return (
     <div className="card">
-      <img src="../uploads/profilepic" alt="profile"></img>
+      <img src={profilepic} alt="profile"></img>
       <h1>Hi, I'm <span>Shiba</span></h1>
-      <p className="role">Front-End Web Developer - Afghanistan</p>
+      <p className="role">Front-End Web Developer</p>
       <p className="desc">Specializes in creating responsive, user-friendly interfaces using HTML, CSS, and JavaScript. Focused on delivering seamless user experiences through modern web technologies.</p>
       <div className="social">
-        <div>
-          <img src="../uploads/github" alt="github icon"></img>
+        <div className="iconcard">
+          <img src={github} alt="github icon"></img>
         </div>
-        <div>
-          <img src="uploads/twitter" alt="twitter icon"></img>
+        <div className="iconcard">
+          <img src={twitter} alt="twitter icon"></img>
         </div>
-        <div>
-          <img src="uploads/tiktok" alt="tiktok icon"></img>
+        <div className="iconcard">
+          <img src={tiktok} alt="tiktok icon"></img>
         </div>
-        <div>
-          <img src="uploads/whatsapp" alt="whatsapp icon"></img>
+        <div className="iconcard">
+          <img src={whatsapp} alt="whatsapp icon"></img>
         </div>
       </div>
     </div>
